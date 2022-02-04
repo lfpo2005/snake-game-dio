f5ba7325ea27932e4a5fe6d29c0d31a341e71918
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2022-02-04 16:00:34 -0300

    drawFood

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1)* box
+}
 
 function criarBG(){
     context.fillStyle = "Lightgreen";
@@ -19,6 +23,13 @@ function criarSnake(){
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+function drawFood(){
+
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+
+}
+
 
 document.addEventListener("keydown", update);
 
@@ -38,6 +49,7 @@ function iniciarJogo(){
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     criarBG();
     criarSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
