b0b9b86793e430df6f1aa4f76911683a90de3637
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2022-02-04 16:45:56 -0300

    css

--- js/main.js
+++ js/main.js
@@ -42,11 +42,19 @@ function update(event) {
 }
 
 function iniciarJogo(){
-
+    
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+   
+   for(i = 1; i < snake.length; i++){
+       if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+           clearInterval(jogo);
+           alert("GAMER OVER :(")
+       }
+   }
+   
     criarBG();
     criarSnake();
     drawFood();
@@ -77,4 +85,4 @@ function iniciarJogo(){
 
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 130);
